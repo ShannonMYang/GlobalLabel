9c2efa2a299f65130d71cb802f43f028e170ee48
杨秀坤 <myang.shannon1122.gmail.com>
2018-07-02 20:29:02 +0800

    Initial Commit

---
+++ GlobalLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GlobalLabel
+//
+//  Created by Shannon MYang on 2018/7/2.
+//  Copyright © 2018年 Shannon MYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GlobalLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GlobalLabel
+//
+//  Created by Shannon MYang on 2018/7/2.
+//  Copyright © 2018年 Shannon MYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
